0c069ce538931488b8967e995b6b142b9e47bec3
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2019-07-24 09:46:55 -0700

    Create updateweb.sh

---
+++ updateweb.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+NAME="Bob"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+# mkdir hello
+# touch "hello/world.txt"
+# echo "Hello World" >> "hello/world.txt"
+# echo "Created hello/world.txt"
